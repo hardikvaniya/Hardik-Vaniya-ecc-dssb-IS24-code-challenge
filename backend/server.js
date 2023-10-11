d4d1ddb326e46a914594ecc6baf00325ff52abf6
hardikvaniya <hardikvaniya3800@gmail.com>
2023-10-10 17:53:49 -0700

    Add POST product API

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const cors = require('cors'); // Import the 'cors' middleware
 const app = express();
-const usersRoute = require('./src/routes/products');
+const productsRoute = require('./src/routes/products');
+const productRoute = require('./src/routes/product');
 
 const port = process.env.PORT || 5000;
 const setupSwagger = require('./src/swagger/swagger.js'); // Replace with the correct path
 
 // ... Other middleware and routes ...
+app.use(express.json());
 app.use(cors());
-app.use('/api', usersRoute);
+
+app.use('/api', productsRoute);
+app.use('/api', productRoute);
 
 // Initialize Swagger documentation
 setupSwagger(app);
--- backend/src/routes/product.js
+++ backend/src/routes/product.js
@@ -1,20 +1,127 @@
+const fs = require('fs');
 const express = require('express');
+const path = require('path');
 const router = express.Router();
+
+function addProductToFile(productName, productOwnerName, developers, scrumMasterName, startDate, methodology, location) {
+    // Read the existing data from the JSON file
+    const filePath = path.join(__dirname, '../mock_data/products.json');
+    const rawData = fs.readFileSync(filePath);
+    const data = JSON.parse(rawData);
+
+    // Find the highest existing productId
+    let highestProductId = 0;
+    for (const product of data) {
+        if (product.productId > highestProductId) {
+            highestProductId = product.productId;
+        }
+    }
+
+    // Increment the highest productId by one to create a new productId
+    const newProductId = highestProductId + 1;
+
+    // Create a new product object with the provided values
+    const newProduct = {
+        productId: newProductId,
+        productName,
+        productOwnerName,
+        developers,
+        scrumMasterName,
+        startDate,
+        methodology,
+        location,
+    };
+
+    // Add the new product to the existing data
+    data.push(newProduct);
+
+    // Write the updated data back to the JSON file
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+
+    return data;
+}
 
 /**
  * @swagger
- * /api/products:
- *   get:
- *     summary: Get a list of products
- *     description: Retrieve a list of products from the database.
+ * /api/product:
+ *   post:
+ *     summary: Create a new product
+ *     description: Add a new product to the database.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:   # Change content type to JSON
+ *           schema:
+ *             type: object
+ *             properties:
+ *               productName:
+ *                 type: string
+ *                 description: The name of the product.
+ *               productOwnerName:
+ *                 type: string
+ *                 description: The name of the product owner.
+ *               developers:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *                 description: An array of developer names.
+ *               scrumMasterName:
+ *                 type: string
+ *                 description: The name of the Scrum Master.
+ *               startDate:
+ *                 type: string
+ *                 format: date
+ *                 description: The start date of the product.
+ *               methodology:
+ *                 type: string
+ *                 description: The development methodology (e.g., Agile, Waterfall).
+ *               location:
+ *                 type: string
+ *                 description: The location of the product.
+ *     consumes:
+ *       - application/json   # Update the content type to JSON
  *     responses:
- *       '200':
- *         description: A successful response with a list of products.
+ *       '201':
+ *         description: Product created successfully.
+ *       '400':
+ *         description: Bad request, data validation failed.
+ *       '500':
+ *         description: Error adding the product to the database.
  */
-router.get('/product/:id', (req, res) => {
-    const productId = req.params.id; // Access the 'id' parameter from the URL
-    // Your code to retrieve a single product by ID from the database
-    res.json({ product: productId }); // Replace with actual data
+router.post('/product', (req, res) => {
+    // Verify that all required values are present in req.body
+    const {
+        productName,
+        productOwnerName,
+        developers,
+        scrumMasterName,
+        startDate,
+        methodology,
+        location
+    } = req.body;
+
+    if (
+        !productName ||
+        !productOwnerName ||
+        !developers ||
+        !scrumMasterName ||
+        !startDate ||
+        !methodology ||
+        !location
+    ) {
+        // If any of the required values are missing, return a 400 bad request response
+        return res.status(400).json({ error: 'Missing required fields' });
+    }
+
+    // Add the product to the database and wrap it in try/catch in case of errors
+    try {
+        addProductToFile(productName, productOwnerName, developers, scrumMasterName, startDate, methodology, location);
+    } catch (error) {
+        console.error('Error adding product:', error);
+        return res.status(500).json({ error: 'Error adding product to the database' });
+    }
+
+    res.status(201).json({ message: 'Product created successfully' });
 });
 
 module.exports = router;
